12e868e7165ee034d7ff67d733d06417886c853a
joana.godoy@g.globo <@Central3272526>
2024-01-12 09:52:17 -0300

    Predicate

---
+++ src/_Predicate.java
@@ -0,0 +1,33 @@
+import java.util.function.Predicate;
+
+public class _Predicate {
+
+    public static void main(String[] args) {
+        System.out.println("Without Predicate");
+        System.out.println(isNumberValid("070000"));
+        System.out.println(isNumberValid("000000"));
+
+        System.out.println("With Predicate");
+        System.out.println(isNumberValidPredicate.test("070000"));
+        System.out.println(isNumberValidPredicate.test("000000"));
+
+
+        System.out.println("With And");
+        System.out.println("isNumberValid and ContainsNumber3 = " +
+                isNumberValidPredicate.and(containsNumber3).test("070000"));
+        System.out.println("isNumberValid and ContainsNumber3 = " +
+                isNumberValidPredicate.and(containsNumber3).test("070300"));
+        System.out.println("isNumberValid or ContainsNumber3 = " +
+                isNumberValidPredicate.or(containsNumber3).test("070000"));
+
+    }
+    static Predicate<String> isNumberValidPredicate = phoneNumber ->
+            phoneNumber.startsWith("07");
+
+    static Predicate<String> containsNumber3 = phoneNumber ->
+            phoneNumber.contains("3");
+
+    static boolean isNumberValid(String phoneNumber){
+        return phoneNumber.startsWith("07");
+    }
+}
